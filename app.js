c6e6a104d0b07e34799d6ecd9de9c55dde109878
yashvanth5 <116138513+yashvanth5@users.noreply.github.com>
2022-11-22 22:31:58 +0530

    refactore done

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var btnTranslate = document.querySelector("#btn-txt");
 var textInput = document.querySelector("#text-input");
-var outputDiv = document.querySelector("#output");
-
+var outputDiv = document.querySelector("#output-txt");
 var severUrl = "https://api.funtranslations.com/translate/pirate.json"
 
-function getTranslatedUrl(input) {
-    return severUrl + "?" + "text= " + input
-
+function getTranslatedUrl(text) 
+{
+    return severUrl + "?" + "text= " + text
 }
 
-function errorHandling(error) {
+function errorHandler(error) 
+{
 
-    console.log("error occured", error)
+    console.log("error occured", error);
     alert("something wrong with server try again later")
 
 }
@@ -21,14 +21,19 @@ function errorHandling(error) {
 
 
 
-function clickEventHandler() {
-    // outputDiv.innerText = "translation: " + textInput.value;
+function clickEventHandler() 
+{
     var inputText = textInput.value;
+
     fetch(getTranslatedUrl(inputText))
     .then(response => response.json())
-    .then(json => outputDiv.innerText = json.contents.translated)
-    .catch(errorHandling)
-}
+    .then(json => {
+        var outputText = json.contents.translated; 
+       outputDiv.innertext = outputText;
+        } )
+        
+    .catch(errorHandler)
+};
 
 
 
