c4750aa8b497c4f0e59dcf6a8d65b4a8f9a98bc3
yashvanth5 <116138513+yashvanth5@users.noreply.github.com>
2022-12-01 19:10:23 +0530

    finally done

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
+// var btnTranslate = document.querySelector("#btn-txt");
+// var textInput = document.querySelector("#text-input");
+// var outputDiv = document.querySelector("#output-txt");
+
+// var severUrl = "https://api.funtranslations.com/translate/pirate.json"
+
+// function getTranslation(text) 
+// {
+//     return severUrl + "?" + "text=" + text
+    
+// }
+
+// function errorHandler(error) {
+//     console.log("error occured", error);
+//     alert("something wrong with server try again later")
+//      }
+
+// function clickEventHandler() {
+//     var inputText = textInput.value;
+//     fetch(getTranslation(inputText))
+//     .then(response => response.json())
+//     .then(json => {
+//         var outputText = json.contents.translated; 
+//        outputDiv.innerText = outputText;
+//         } )
+//         .catch(errorHandler)
+// };
+
+
+
+// btnTranslate.addEventListener("click", clickEventHandler);
+
+
+
 var btnTranslate = document.querySelector("#btn-txt");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output-txt");
 
-var severUrl = "https://api.funtranslations.com/translate/pirate.json"
+var serverUrl = "https://api.funtranslations.com/translate/pirate.json"; // changed spelling of "server"
 
-function getTranslation(text) 
-{
-    return severUrl + "?" + "text=" + text
-    
+function getTranslation(text) {
+    return serverUrl + "?" + "text=" + encodeURIComponent(text); // added `encodeURIComponent()` to properly encode the text
 }
 
 function errorHandler(error) {
-    console.log("error occured", error);
-    alert("something wrong with server try again later")
-     }
+    console.log("error occurred", error); // corrected spelling of "occurred"
+    alert("something went wrong with the server. Please try again later."); // added more details to alert message
+}
 
 function clickEventHandler() {
     var inputText = textInput.value;
     fetch(getTranslation(inputText))
-    .then(response => response.json())
-    .then(json => {
-        var outputText = json.contents.translated; 
-       outputDiv.innerText = outputText;
-        } )
-        .catch(errorHandler)
-};
-
-
+        .then(response => response.json())
+        .then(json => {
+            var outputText = json.contents.translated;
+            outputDiv.innerText = outputText;
+        })
+        .catch(errorHandler);
+}
 
 btnTranslate.addEventListener("click", clickEventHandler);
